b1b4d5c04cf915f96849c98423b63e57e5834702
WilliamLiu-1997 <77861330+WilliamLiu-1997@users.noreply.github.com>
2021-09-19 16:44:09 +1000

    up

---
+++ Assets/Scripts/Pac_Student_Direction_Controller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pac_Student_Direction_Controller : MonoBehaviour
+{
+    Animator Pac_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pac_Animator=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            Pac_Animator.SetInteger("State",1);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            Pac_Animator.SetInteger("State",2);
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            Pac_Animator.SetInteger("State",3);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            Pac_Animator.SetInteger("State",4);
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Pac_Animator.SetInteger("State",5);
+        }
+    }
+}
