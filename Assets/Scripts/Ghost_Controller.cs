dec33ec123e824d3bd146b5b10cdd45b88dbb44b
WilliamLiu-1997 <77861330+WilliamLiu-1997@users.noreply.github.com>
2021-09-19 23:45:07 +1000

    u

--- Assets/Scripts/Ghost_Controller.cs
+++ Assets/Scripts/Ghost_Controller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ghost_Controller : MonoBehaviour
 {
     Animator Ghost_Animator;
+    private float duration_Normal;
     private float duration_Scare;
     private float duration_Awake;
     private float duration_Die;
@@ -12,6 +13,7 @@ public class Ghost_Controller : MonoBehaviour
     void Start()
     {
         Ghost_Animator=GetComponent<Animator>();
+        duration_Normal=0;
         duration_Scare=-1;
         duration_Awake=-1;
         duration_Die=-1;
@@ -20,8 +22,10 @@ public class Ghost_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        //if(Input.GetKeyDown(KeyCode.Space)){
+        if(duration_Normal>=3){
             Ghost_Animator.SetTrigger("Scare");
+            duration_Normal=-1;
             duration_Scare=0;
         }
         if(duration_Scare>=3){
@@ -37,7 +41,9 @@ public class Ghost_Controller : MonoBehaviour
         if(duration_Die>=3){
             Ghost_Animator.SetTrigger("Recover");
             duration_Die=-1;
+            duration_Normal=0;
         }
+        if(duration_Normal>=0)duration_Normal+=Time.deltaTime;
         if(duration_Scare>=0)duration_Scare+=Time.deltaTime;
         if(duration_Awake>=0)duration_Awake+=Time.deltaTime;
         if(duration_Die>=0)duration_Die+=Time.deltaTime;
